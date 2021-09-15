fe92d358ab1e88f63a45e98a807c7fb2ce9f186b
Jesus Silva <jjesussh31@gmail.com>
2021-09-15 16:28:51 -0500

    My first part of my implementation

---
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaoTest.kt
@@ -0,0 +1,13 @@
+package com.androiddevs.shoppinglisttestingyt.data.local
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class ShoppingDaoTest {
+    private lateinit var database: ShoppingItemDatabase
+
+
+}
